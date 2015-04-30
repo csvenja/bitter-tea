5a441919203f50b74ecd1751d26c9539c8eeb2cf
Svenja Cao <c.svenjax@gmail.com>
2015-04-29 20:28:18 -0700

    Update model.

--- kaushue/models.py
+++ kaushue/models.py
@@ -4,16 +4,14 @@ from django.db import models
 class Question(models.Model):
     title = models.CharField(max_length=200)
     content = models.CharField(max_length=100000)
-    reference = models.ManyToManyField("self", symmetrical=False, blank=True)
+    reference = models.ManyToManyField(
+        "self", through="Connection", symmetrical=False, blank=True)
 
     def __unicode__(self):
         return self.title
 
 
 class Connection(models.Model):
-    from_question = models.IntegerField()
-    to_question = models.IntegerField()
+    from_question = models.ForeignKey(Question, related_name="from_question")
+    to_question = models.ForeignKey(Question, related_name="to_question")
     logic = models.CharField(max_length=100000)
-
-    def __unicode__(self):
-        return
--- kaushue/urls.py
+++ kaushue/urls.py
@@ -4,5 +4,5 @@ from kaushue import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<page_id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
 )
