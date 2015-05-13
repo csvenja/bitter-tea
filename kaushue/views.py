9274619bb28f9478d4a44737c9b8ef22a1021ae0
LIU Dongyuan / 柳东原 <liu.dongyuan@gmail.com>
2015-05-13 00:52:56 -0700

    Able to add question

--- kaushue/views.py
+++ kaushue/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import get_object_or_404, render
+# -*- coding: utf-8 -*-
+
+from django.shortcuts import get_object_or_404, render, redirect
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
@@ -49,7 +51,23 @@ def edit(request, question_id):
     question_id = int(question_id)
     content = request.POST.get('content', None)
     question = get_object_or_404(Question, pk=question_id)
-    print question
     question.content = content
     question.save()
     return HttpResponse()
+
+
+def new(request):
+    if request.method == 'GET':
+        return render(request, 'new.html')
+    elif request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        if title and content:
+            Question.objects.create(title=title, content=content)
+            return redirect('/')
+        else:
+            return render(request, 'new.html', {
+                'title': title,
+                'content': content,
+                'error': '请填写标题和内容'
+            })
